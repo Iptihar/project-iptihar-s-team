e6906c636811c83c5708b999ab46f0d6fbb4d2a2
Iptihar Osman <tiamotulip@gmail.com>
2017-11-26 04:10:27 -0600

    CMYK colors fixed

--- CMT/CMT.py
+++ CMT/CMT.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from PIL import Image
 from datetime import datetime
 
 class ColorModelTransformation:
@@ -8,76 +9,43 @@ class ColorModelTransformation:
         """input: rgb color image
             for GUI: need a round radio option button with label:  CYAN
             output: cyan color image """
-        rows, cols, chans = image.shape
+        im = Image.fromarray(image.astype('uint8'), 'RGB')
+        output = im.convert("CMYK")
+        output1 = np.array(output)
+        cyan = output1[:,:,2]
+        im = Image.new("CMYK", (cyan.shape[1], cyan.shape[0]), "white")
+        output2 = np.array(im)
+        output2[:,:,2] = cyan
 
-        red = image[:, :, 2]
-        green = image[:, :, 1]
-        blue = image[:, :, 0]
-
-        output = np.ones((rows, cols, 4), np.uint8)
-        for i in range(rows):
-            for j in range(cols):
-                r = 1 - red[i, j] / 255
-                g = 1 - green[i, j] / 255
-                b = 1 - blue[i, j] / 255
-                k = min(r,g,b)
-
-                output[i, j, 0] = (r - k) / (1 - k) * 300.
-                output[i, j, 1] = (g - k) / (1 - k) * 300.
-                output[i, j, 2] = 0
-                output[i, j, 3] = k * 300.
-
-        return output
+        return output2
 
     def get_magenta(self, image):
         """input: rgb color image
             for GUI: need a round radio option button with label:  MAGENTA
             output: magenta color image """
-        rows, cols, chans = image.shape
+        im = Image.fromarray(image.astype('uint8'), 'RGB')
+        output = im.convert("CMYK")
+        output1 = np.array(output)
+        cyan = output1[:, :, 1]
+        im = Image.new("CMYK", (cyan.shape[1], cyan.shape[0]), "white")
+        output2 = np.array(im)
+        output2[:, :, 1] = cyan
 
-        red = image[:, :, 2]
-        green = image[:, :, 1]
-        blue = image[:, :, 0]
-
-        output = np.ones((rows, cols, 4), np.uint8)
-        for i in range(rows):
-            for j in range(cols):
-                r = 1 - red[i, j] / 255
-                g = 1 - green[i, j] / 255
-                b = 1 - blue[i, j] / 255
-                k = min(r, g, b)
-
-                output[i, j, 0] = (r - k) / (1 - k) * 300.
-                output[i, j, 1] = 0
-                output[i, j, 2] = (b - k) / (1 - k) * 300.
-                output[i, j, 3] = k * 300.
-
-        return output
+        return output2
 
     def get_yellow(self, image):
         """input: rgb color image
             for GUI: need a round radio option button with label:  YELLOW
             output: yellow color image """
-        rows, cols, chans = image.shape
+        im = Image.fromarray(image.astype('uint8'), 'RGB')
+        output = im.convert("CMYK")
+        output1 = np.array(output)
+        cyan = output1[:, :, 0]
+        im = Image.new("CMYK", (cyan.shape[1], cyan.shape[0]), "white")
+        output2 = np.array(im)
+        output2[:, :, 0] = cyan
 
-        red = image[:, :, 2]
-        green = image[:, :, 1]
-        blue = image[:, :, 0]
-
-        output = np.ones((rows, cols, 4), np.uint8)
-        for i in range(rows):
-            for j in range(cols):
-                r = 1 - red[i, j] / 255
-                g = 1 - green[i, j] / 255
-                b = 1 - blue[i, j] / 255
-                k = min(r, g, b)
-
-                output[i, j, 0] = 0
-                output[i, j, 1] = (g - k) / (1 - k) * 300.
-                output[i, j, 2] = (b - k) / (1 - k) * 300.
-                output[i, j, 3] = k * 300.
-
-        return output
+        return output2
 
     def get_black(self, image):
         """input: rgb color image
@@ -167,13 +135,13 @@ def main():
     #Write output file
     output_dir = 'output/'
         
-    output_image = cmt.get_black(input_image)
+    output_image = cmt.get_cyan(input_image)
 
     cv2.imshow("Lenna", output_image)
     cv2.waitKey(0)
 
-    output_image_name = output_dir + "_BLACK_" + datetime.now().strftime("%m%d-%H%M%S")+".jpg"
-    cv2.imwrite(output_image_name, output_image)
+#    output_image_name = output_dir + "_BLACK_" + datetime.now().strftime("%m%d-%H%M%S")+".jpg"
+#    cv2.imwrite(output_image_name, output_image)
 
 
 if __name__ == "__main__":
